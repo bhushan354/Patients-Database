106036ea47d43412d8978790dbb8ae2169755d9e
bhushan354 <deshmukhumesh354@gmail.com>
2023-10-31 21:46:53 +0530

    Add indexes to foreign keys

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -59,3 +59,15 @@ CREATE TABLE invoice_items (
 
 ALTER TABLE invoice_items
 ADD CONSTRAINT fk_invoices_invoiceItems FOREIGN KEY (invoice_id) REFERENCES invoices(id);
+
+-- Add a B-tree index to the "patient_id" foreign key in the "medical_histories" table
+CREATE INDEX idx_patient_id ON medical_histories(patient_id);
+
+-- Add a B-tree index to the "id" foreign key in the "treatments" table
+CREATE INDEX idx_medical_history_id ON treatments(id);
+
+-- Add a B-tree index to the "treatment_id" foreign key in the "invoice_items" table
+CREATE INDEX idx_treatment_id ON invoice_items(treatment_id);
+
+-- Add a B-tree index to the "medical_history_id" foreign key in the "invoices" table
+CREATE INDEX idx_medical_history_id ON invoices(medical_history_id);
