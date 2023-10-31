6b4284798ee5d33d19e56a7dc36cc545f20a8dae
bhushan354 <deshmukhumesh354@gmail.com>
2023-10-31 17:04:45 +0530

    Changes

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -10,3 +10,12 @@ CREATE TABLE patients (
     date_of_birth DATE
 );
 
+-- create medical_histories table and add foreign key
+
+CREATE TABLE medical_histories (
+    id INT PRIMARY KEY,
+    admitted_at TIMESTAMP,
+    patient_id INT,
+    status VARCHAR(255),
+    CONSTRAINT fk_medical_histories FOREIGN KEY (patient_id) REFERENCES patients(id)
+);
