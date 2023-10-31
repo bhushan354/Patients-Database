d6bfe952d8e7ec0805e95c99f5b882a61a16d96c
bhushan354 <deshmukhumesh354@gmail.com>
2023-10-31 14:42:01 +0530

    Add patients table

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -1,3 +1,12 @@
 -- make changes 
 -- add gitflow
 -- add README
+
+-- create patients table :
+
+CREATE TABLE patients (
+    id INT PRIMARY KEY,
+    name VARCHAR(255),
+    date_of_birth DATE
+);
+
